4528ae074e24e0f716860125136371fe8f0b26c7
yellowmonkeyman <60224626+yellowmonkeyman@users.noreply.github.com>
2020-01-28 15:51:28 +0000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,32 @@
+// let myVariable = "Richard";
+// myVariable = "Rik";
+
+// console.log(`Hello everyone! My name is ${myVariable}`);
+
+// let myVariable1 = 100;
+
+// console.log(myVariable1);
+// console.log(`The number I have selected is ${myVariable1}`);
+
+// let myVariable2 = true;
+// let myVariable3 = true;
+
+// let variable2 = 10;
+// let variable3 = 20;
+
+// console.log(myVariable3);
+
+// function newPhone(brand, model) {
+//     console.log(`The new ${brand} ${model} is now released`)
+// }
+
+// newPhone("iPhone", "1 million");
+
+let favouriteAnimals = [
+    'Snail',
+    'Fox',
+    'Cow'
+];
+
+console.log(favouriteAnimals)
+console.log(favouriteAnimals[1])
